e70a11c582edac60c04a3db699b26a64535757b2
Phil Monroe <phil@philmonroe.com>
2014-08-03 18:54:22 -0700

    getting heroku working

--- project/Git.scala
+++ project/Git.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Git {
-  lazy val GitSha = Option(System.getenv("GIT_SHA")).getOrElse(Process("git rev-parse HEAD").lines.head)
+  lazy val GitSha = "" //Option(System.getenv("GIT_SHA")).getOrElse(Process("git rev-parse HEAD").lines.head)
   lazy val GitShaShort = GitSha.substring(0, 8)
 
   def version(baseVersion: String) = {
