47020a5afd451bb4989528f063c9a12059a4c1ae
Phil Monroe <phil@philmonroe.com>
2014-08-03 18:16:57 -0700

    tweaking Git.scala

--- project/Git.scala
+++ project/Git.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Git {
-  lazy val GitSha = Process("git rev-parse HEAD").lines.head
+  lazy val GitSha = Process("git rev-parse HEAD 2>/dev/null || echo $GIT_SHA").lines.head
   lazy val GitShaShort = GitSha.substring(0, 8)
 
   def version(baseVersion: String) = {
--- src/main/scala/example/philmonroe/setup/bundles/TweetStreamBundle.scala
+++ src/main/scala/example/philmonroe/setup/bundles/TweetStreamBundle.scala
@@ -4,12 +4,10 @@ import io.dropwizard.ConfiguredBundle
 import io.dropwizard.setup.{Environment, Bootstrap}
 import example.philmonroe.DwExampleConfig
 import example.philmonroe.core.tweet_stream.{TweetProcessor, TweetProcessingPool, ManagedTweetStream}
+import example.philmonroe.healthchecks.TweetStreamHealthcheck
 
 class TweetStreamBundle(esBundle: ElasticSearchBundle) extends ConfiguredBundle[DwExampleConfig] {
-
-  override def initialize(bootstrap: Bootstrap[_]): Unit = {
-
-  }
+  override def initialize(bootstrap: Bootstrap[_]): Unit = {}
 
   override def run(config: DwExampleConfig, env: Environment): Unit = {
     val tweetStream = new ManagedTweetStream(config.twitter)
@@ -19,5 +17,7 @@ class TweetStreamBundle(esBundle: ElasticSearchBundle) extends ConfiguredBundle[
     env.lifecycle().manage(processorPool)
     processors.foreach(env.lifecycle().manage)
     env.lifecycle().manage(tweetStream)
+
+    env.healthChecks().register("tweet-stream", new TweetStreamHealthcheck(tweetStream))
   }
 }
