434ae6433eba0748cf74d191356b7533a9b913a6
Phil Monroe <phil@philmonroe.com>
2014-08-04 00:02:46 -0700

    remove unneeded import

--- src/main/scala/example/philmonroe/setup/config/TwitterConfig.scala
+++ src/main/scala/example/philmonroe/setup/config/TwitterConfig.scala
@@ -1,6 +1,5 @@
 package example.philmonroe.setup.config
 
-import javax.validation.constraints.NotNull
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.hibernate.validator.constraints.NotEmpty
 
